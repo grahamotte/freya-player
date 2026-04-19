202dbcb126a4a8f653cdeec03eba614c37407305
Graham Otte <go@graham.lol>
2026-04-19 10:09:39 -0700

    Set playback audio session at launch

--- FreyaPlayer/FreyaPlayerApp.swift
+++ FreyaPlayer/FreyaPlayerApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import AVFAudio
 
 @main
 struct FreyaPlayerApp: App {
+    init() {
+        try? AVAudioSession.sharedInstance().setCategory(.playback, mode: .moviePlayback)
+    }
+
     var body: some Scene {
         WindowGroup {
             AppView()
