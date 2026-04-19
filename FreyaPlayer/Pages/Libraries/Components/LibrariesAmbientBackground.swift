54313e19dbd3688b306264e81b0db49cf7b67a26
Graham Otte <go@graham.lol>
2026-04-19 11:44:19 -0700

    Randomize library background mesh colors

--- FreyaPlayer/Pages/Libraries/Components/LibrariesAmbientBackground.swift
+++ FreyaPlayer/Pages/Libraries/Components/LibrariesAmbientBackground.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct LibrariesAmbientBackground: View {
+    @State private var colors = Self.makeColors()
+
     var body: some View {
         ZStack {
             AppBackground()
@@ -12,27 +14,7 @@ struct LibrariesAmbientBackground: View {
                     width: 4,
                     height: 4,
                     points: meshPoints(at: time),
-                    colors: [
-                        Color(red: 0.98, green: 0.28, blue: 0.54),
-                        Color(red: 0.76, green: 0.22, blue: 0.82),
-                        Color(red: 0.34, green: 0.29, blue: 0.96),
-                        Color(red: 0.19, green: 0.52, blue: 1.0),
-
-                        Color(red: 1.0, green: 0.56, blue: 0.23),
-                        Color(red: 0.94, green: 0.2, blue: 0.42),
-                        Color(red: 0.68, green: 0.23, blue: 0.84),
-                        Color(red: 0.21, green: 0.62, blue: 1.0),
-
-                        Color(red: 0.96, green: 0.36, blue: 0.27),
-                        Color(red: 1.0, green: 0.27, blue: 0.56),
-                        Color(red: 0.23, green: 0.85, blue: 0.58),
-                        Color(red: 0.18, green: 0.73, blue: 0.88),
-
-                        Color(red: 1.0, green: 0.58, blue: 0.24),
-                        Color(red: 0.82, green: 0.41, blue: 0.22),
-                        Color(red: 0.22, green: 0.86, blue: 0.56),
-                        Color(red: 0.2, green: 0.54, blue: 1.0)
-                    ]
+                    colors: colors
                 )
                 .hueRotation(.degrees(sin(time * 0.05) * 10))
                 .blur(radius: 120)
@@ -118,5 +100,41 @@ struct LibrariesAmbientBackground: View {
 
     private func clamp(_ value: Float) -> Float {
         min(max(value, 0), 1)
+    }
+
+    private static func makeColors() -> [Color] {
+        let baseHue = Double.random(in: 0...1)
+        let hueSpread = Double.random(in: 0.45...0.8)
+        let hueOffsets = [
+            -0.34, -0.2, -0.08, 0.04,
+            0.18, -0.12, 0.28, 0.42,
+            0.1, 0.24, 0.54, 0.36,
+            0.48, 0.64, 0.76, 0.9
+        ]
+        let saturations = [
+            0.72, 0.78, 0.8, 0.76,
+            0.82, 0.84, 0.76, 0.72,
+            0.8, 0.74, 0.7, 0.78,
+            0.82, 0.76, 0.72, 0.8
+        ]
+        let brightnesses = [
+            0.98, 0.88, 0.93, 0.97,
+            0.96, 0.86, 0.92, 0.95,
+            0.94, 0.98, 0.9, 0.87,
+            0.96, 0.89, 0.94, 0.97
+        ]
+
+        return hueOffsets.enumerated().map { index, offset in
+            Color(
+                hue: wrappedHue(baseHue + (offset * hueSpread)),
+                saturation: saturations[index],
+                brightness: brightnesses[index]
+            )
+        }
+    }
+
+    private static func wrappedHue(_ value: Double) -> Double {
+        let wrapped = value.truncatingRemainder(dividingBy: 1)
+        return wrapped >= 0 ? wrapped : wrapped + 1
     }
 }
